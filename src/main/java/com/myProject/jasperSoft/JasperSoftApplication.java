fe749ee98b533365aad4481a3df2583893ff5091
Surbhi Girdhani <surbhigirdhani@gmail.com>
2024-04-03 12:30:36 +0530

    Initial changes

---
+++ src/main/java/com/myProject/jasperSoft/JasperSoftApplication.java
@@ -0,0 +1,13 @@
+package com.myProject.jasperSoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasperSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasperSoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myProject/jasperSoft/JasperSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myProject.jasperSoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasperSoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
